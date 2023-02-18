ed8f12231c0d78a2743b45bb84533f3569c35e63
Luke Barnett <ljb2208@gmail.com>
2023-02-18 16:20:49 -0500

    Updates

--- smodels/model.py
+++ smodels/model.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from functools import partial
+import math
 
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 
@@ -35,35 +36,147 @@ class PatchEmbed(nn.Module):
 
         return x, (H, W)
 
-class Attention(nn.Module):
-    def __init__(self, dim, num_embeddings=64, qkv_bias=False, attn_drop=0., proj_drop=0., sr_ratio=1):
-        super().__init__()        
+class SegOverlapPatchEmbed(nn.Module):
+    """ Image to Patch Embedding
+    """
 
-        self.dim = dim        
-        k_dim = num_embeddings // (sr_ratio*sr_ratio)
+    def __init__(self, img_size=224, patch_size=7, stride=4, in_chans=3, embed_dim=768):
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
 
-        self.q = nn.Conv1d(num_embeddings, num_embeddings, kernel_size=1)
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.H, self.W = img_size[0] // patch_size[0], img_size[1] // patch_size[1]
+        self.num_patches = self.H * self.W
+        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=stride,
+                              padding=(patch_size[0] // 2, patch_size[1] // 2))
+        self.norm = nn.LayerNorm(embed_dim)
 
-        self.k = nn.Conv2d(num_embeddings, num_embeddings, kernel_size=sr_ratio, stride=sr_ratio, bias=qkv_bias)
-        self.kbn = nn.BatchNorm1d(num_embeddings)
-        self.k2 = nn.Conv1d(num_embeddings, num_embeddings, kernel_size=1)
+        self.apply(self._init_weights)
 
-        self.q2 = nn.Conv1d(num_embeddings, num_embeddings, kernel_size=1, stride=1)
-        self.v = nn.AvgPool1d(num_embeddings)
-        self.rwmp = nn.MaxPool1d(k_dim)        
-                
-        self.conv = nn.Conv1d(num_embeddings, num_embeddings, kernel_size=1, bias=qkv_bias)
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.proj(x)
+        _, _, H, W = x.shape
+        x = x.flatten(2).transpose(1, 2)
+        x = self.norm(x)
+
+        return x, H, W
+
+class SegAttention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0., sr_ratio=1):
+        super().__init__()
+        assert dim % num_heads == 0, f"dim {dim} should be divided by num_heads {num_heads}."
+
+        self.dim = dim
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.q = nn.Linear(dim, dim, bias=qkv_bias)
+        self.kv = nn.Linear(dim, dim * 2, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
 
         self.sr_ratio = sr_ratio
+        if sr_ratio > 1:
+            self.sr = nn.Conv2d(dim, dim, kernel_size=sr_ratio, stride=sr_ratio)
+            self.norm = nn.LayerNorm(dim)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+    def forward(self, x, H, W):
+        B, N, C = x.shape
+        q = self.q(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+
+        if self.sr_ratio > 1:
+            x_ = x.permute(0, 2, 1).reshape(B, C, H, W)
+            x_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1)
+            x_ = self.norm(x_)
+            kv = self.kv(x_).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        else:
+            kv = self.kv(x).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        k, v = kv[0], kv[1]
+
+        print(q.shape)
+        print(k.shape)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        print(attn.shape)
+        print(v.shape)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+
+        return x
+
+class Attention(nn.Module):
+    def __init__(self, num_embeddings=64, sr_ratio=1, num_heads=1):
+        super().__init__()        
+               
+        self.num_heads = num_heads
+        self.num_embeddings = num_embeddings
+        self.sr_ratio = sr_ratio
+        self.scale = 1. / (sr_ratio*sr_ratio)
+        #k_dim = num_embeddings // sr_ratio
+
+        self.q = nn.Conv1d(num_embeddings, num_embeddings, kernel_size=1, bias=False)
+
+        self.k = nn.Conv2d(num_embeddings, num_embeddings, kernel_size=sr_ratio, stride=sr_ratio)
+        self.kbn = nn.BatchNorm1d(num_embeddings)
+        self.k2 = nn.Conv1d(num_embeddings, num_embeddings, kernel_size=1, bias=False)
+
+        # self.q2 = nn.Conv1d(num_embeddings, num_embeddings, kernel_size=1, stride=1)
+        # self.v = nn.AvgPool1d(dim)
+        #self.rwmp = nn.MaxPool1d(k_dim)        
+                
+        self.conv = nn.Conv1d(num_embeddings, num_embeddings, kernel_size=1)
+
+        
         #if sr_ratio > 1:
-        self.sr = nn.Conv2d(dim, dim, kernel_size=sr_ratio, stride=sr_ratio)            
+        #self.sr = nn.Conv2d(num_embeddings, num_embeddings, kernel_size=sr_ratio, stride=sr_ratio)            
 
     def forward(self, x, H, W):
         B, C, N = x.shape
 
         q_=self.q(x)
-        q_ = q_.reshape(B, C, H, W)
-        print(q_.shape)
+        q_ = q_.reshape(B, self.num_heads, C // self.num_heads, N)
+        # print(q_.shape)
         # q_ = q_.permute(0,2,1,3)
         # print(q_.shape)
         # q_ = q_.transpose(2,1)
@@ -74,50 +187,54 @@ class Attention(nn.Module):
         k_ = k_.reshape(B, C, -1)
         k_ = self.kbn(k_)
         k_ = self.k2(k_)        
-        k_ = k_.reshape(B, C, H//self.sr_ratio, W//self.sr_ratio)
+        k_ = k_.reshape(B, self.num_heads, C // self.num_heads, N // (self.sr_ratio * self.sr_ratio))
         
 
+        q_ = q_.transpose(-2, -1)
         print(q_.shape)
         print(k_.shape)
-
-
-        attn =(q_ @ k_.transpose(-2, -1))
-
-        x_ = x.reshape(B, C, H, W)
-        q_ = self.q(x_).reshape(B, C, N//C)
-        q_ = self.bn(q_)
-        q_ = self.q2(q_)        
         
-        # x_ = x.permute(0, 2, 1).reshape(B, C, H, W)
-        # x_ = x.reshape(B, C, H, W)
-        k_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1)            
+        attn =(q_ @ k_)
+        attn = torch.mul(attn, self.scale)
+        attn = torch.max(attn, 3)[0]        
+
+        # attn = attn.reshape(B, C, N*self.sr_ratio)
+        # attn = self.rwmp(attn)                        
+
+
+        print(attn.shape)
+
+        v = torch.mean(x, 2).expand(B, 1, self.num_embeddings)
+
+        # v = torch.mean(x, 2).expand(B, 1, self.num_embeddings)
+        # v = torch.tile(v, (B, self.num_embeddings, 1))
+
             
-        v = self.v(x.transpose(2,1)).transpose(1, 2)
+        # v = self.v(x).transpose(2,1)
 
-        attn = (q_ @ k_.transpose(-2, -1)) 
+        print(v.shape)
 
-        attn = self.rwmp(attn)                        
-
-        x = (attn @ v).transpose(1, 2).reshape(B, N, C)                        
+        attn = attn.transpose(2, 1)
+        x = (attn @ v).transpose(2,1)
 
         x = self.conv(x)
 
         return x
 
 class AttentionOrig(nn.Module):
-    def __init__(self, dim, num_features=3136, qkv_bias=False, attn_drop=0., proj_drop=0., sr_ratio=1):
+    def __init__(self, dim, num_embeddings=3136, qkv_bias=False, attn_drop=0., proj_drop=0., sr_ratio=1):
         super().__init__()        
 
         self.dim = dim        
-        k_dim = num_features // (sr_ratio*sr_ratio)
+        k_dim = num_embeddings // (sr_ratio*sr_ratio)
 
-        self.q = nn.Conv2d(num_features, num_features, kernel_size=8, stride=8, bias=qkv_bias)
-        self.v = nn.AvgPool1d(num_features)
+        self.q = nn.Conv2d(num_embeddings, num_embeddings, kernel_size=8, stride=8, bias=qkv_bias)
+        self.v = nn.AvgPool1d(num_embeddings)
         self.rwmp = nn.MaxPool1d(k_dim)
         self.k = nn.Conv1d(k_dim, k_dim, kernel_size=1)        
         self.attn_drop = nn.Dropout(attn_drop)        
         self.proj_drop = nn.Dropout(proj_drop)
-        self.conv = nn.Conv1d(num_features, num_features, kernel_size=1, bias=qkv_bias)
+        self.conv = nn.Conv1d(num_embeddings, num_embeddings, kernel_size=1, bias=qkv_bias)
 
         self.sr_ratio = sr_ratio
         #if sr_ratio > 1:
@@ -169,7 +286,7 @@ class MixFFN(nn.Module):
     def forward(self, x, H, W):
         B, C, N = x.shape
         x = self.conv1(x)
-        x = x.reshape(B, self.hidden_features, H//4, W//4)
+        x = x.reshape(B, self.hidden_features, H, W)
         x = self.bn1(x)
         x = self.dwconv(x)
         x = x.reshape(B, self.hidden_features, N)
@@ -180,53 +297,90 @@ class MixFFN(nn.Module):
         return x
 
 class Block(nn.Module):
-    def __init__(self, dim=64, num_features=3136, sr_ratio=2):
+    def __init__(self, num_embeddings=64, hidden_features=256, sr_ratio=2):
         super().__init__()        
-        self.attn = Attention(dim, num_features=num_features, sr_ratio=sr_ratio)
-        self.ffn = MixFFN(num_features=num_features)
+        self.attn = Attention(num_embeddings, sr_ratio=sr_ratio)
+        self.ffn = MixFFN(num_embeddings=num_embeddings, hidden_features=hidden_features)
 
     def forward(self, x, H, W):
         x = x + self.attn(x, H, W)
-        x = x + self.ffn(x)
+        x = x + self.ffn(x, H, W)
 
         return x
 
 
 class DepthModel(nn.Module):
     def __init__(self, img_size=224, patch_size=16, embed_dims=[64, 128, 250, 320],
-                 depths=[3, 4, 6, 3], sr_ratios=[8, 4, 2, 1], in_chans=3):
+                 depths=[3, 4, 6, 3], sr_ratios=[8, 4, 2, 1], in_chans=3, mlp_ratio=4):
         super().__init__()
 
         self.depths = depths
-        self.embed_dims = embed_dims
+        self.embed_dims = embed_dims        
 
-        for i in range(len(depths)):
-            patch_embed = PatchEmbed(img_size=img_size if i == 0 else img_size // (2 ** (i + 1)), 
-                patch_size=patch_size if i==0 else 2, 
-                embed_dim=embed_dims[i],
-                in_chans=in_chans if i==0 else embed_dims[i-1])
+        self.patch_embed1 = PatchEmbed(img_size=img_size, patch_size=7, stride=4, in_chans=in_chans, 
+                                embed_dim=embed_dims[0])
+        self.patch_embed2 = PatchEmbed(img_size=(img_size[0] // 4, img_size[1] //4), patch_size=3, stride=2, in_chans=embed_dims[0], 
+                                embed_dim=embed_dims[1])
+        self.patch_embed3 = PatchEmbed(img_size=(img_size[0] // 8, img_size[1] //8), patch_size=3, stride=2, in_chans=embed_dims[1], 
+                                embed_dim=embed_dims[2])
+        self.patch_embed4 = PatchEmbed(img_size=(img_size[0] // 16, img_size[1] //16), patch_size=3, stride=2, in_chans=embed_dims[2], 
+                                embed_dim=embed_dims[3])
 
-            block = nn.ModuleList([Block(
-                dim=embed_dims[i], sr_ratio=sr_ratios[i], num_features=patch_embed.H * patch_embed.W)
-                for j in range(depths[i])])
 
-            setattr(self, f"patch_embed{i + 1}", patch_embed)            
-            setattr(self, f"block{i + 1}", block)
+        self.block1 = nn.ModuleList([Block(
+                num_embeddings=embed_dims[0], sr_ratio=sr_ratios[0], hidden_features=embed_dims[0] * mlp_ratio)
+                for j in range(depths[0])])                    
 
+        self.block2 = nn.ModuleList([Block(
+                num_embeddings=embed_dims[1], sr_ratio=sr_ratios[1], hidden_features=embed_dims[1] * mlp_ratio)
+                for j in range(depths[1])])                    
+
+        self.block3 = nn.ModuleList([Block(
+                num_embeddings=embed_dims[2], sr_ratio=sr_ratios[2], hidden_features=embed_dims[2] * mlp_ratio)
+                for j in range(depths[2])])                    
+
+        self.block4 = nn.ModuleList([Block(
+                num_embeddings=embed_dims[3], sr_ratio=sr_ratios[3], hidden_features=embed_dims[3] * mlp_ratio)
+                for j in range(depths[3])])                    
+
+        
     def forward(self, x):
         outs = []
         B = x.shape[0]
 
-        for i in range(len(self.depths)):
-            patch_embed = getattr(self, f"patch_embed{i + 1}")            
-            block = getattr(self, f"block{i + 1}")
+        #stage 1
+        x, (H, W) = self.patch_embed1(x)
 
-            x, (H, W) = patch_embed(x)
+        for i, blk in enumerate(self.block1):
+            x = blk(x, H, W)
 
-            for blk in block:
-                x = blk(x, H, W)         
+        x = x.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+        outs.append(x)
 
-            x = x.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()   
-            outs.append(x)
+        #stage 2
+        x, (H, W) = self.patch_embed2(x)
 
+        for i, blk in enumerate(self.block2):
+            x = blk(x, H, W)
+
+        x = x.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+        outs.append(x)
+
+        #stage 3
+        x, (H, W) = self.patch_embed3(x)
+
+        for i, blk in enumerate(self.block3):
+            x = blk(x, H, W)
+
+        x = x.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+        outs.append(x)
+
+        #stage 4
+        x, (H, W) = self.patch_embed4(x)
+
+        for i, blk in enumerate(self.block4):
+            x = blk(x, H, W)
+
+        x = x.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+        outs.append(x)
         return outs
--- smodels/test2.py
+++ smodels/test2.py
@@ -8,33 +8,75 @@ from overlap_patch_embedding import OverlapPatchEmbedding
 from self_attention import SelfAttention
 from model import PatchEmbed
 from model import Attention
+from model import AttentionOrig
 from model import MixFFN
 from model import Block
 from model import DepthModel
+from model import SegAttention
+from model import SegOverlapPatchEmbed
 
 
-# x = torch.randn((1, 3, 512,1024))
+x = torch.randn((1, 3, 512,1024))
 
-# ope = PatchEmbed(img_size=[512,1024], embed_dim=64, patch_size=7)
+# ope = SegOverlapPatchEmbed(img_size=[512,1024], embed_dim=64)
+# # ope = PatchEmbed(img_size=[512,1024], embed_dim=64, patch_size=7)
 
 # y = ope.forward(x)
 
 # print(y[0].shape)
 # print(y[1])
+# print(y[2])
 
 
+# torch.Size([1, 1, 32768, 64])
+# torch.Size([1, 1, 512, 64])
+# torch.Size([1, 1, 32768, 512])
+# torch.Size([1, 1, 512, 64])
 
-x = torch.randn((1,64, 32768))
+
+# x = torch.randn([1, 1, 32768, 512])
+
+# y = torch.max(x, 3)
+
+# print(y[0].shape)
+
+# x = torch.randn((1,64, 32768))
+
+# y = torch.mean(x, 2)
+# print(y.shape)
+# print(y)
+# y = y.expand(1,1,64)
+
+# y = torch.tile(y, (1,64,1))
+# print(y.shape)
+# print(y)
+
+# x = torch.randn((1,32768, 64))
+# x = torch.randn(1,3,512,1024)
 
 # ffn = MixFFN(num_embeddings=64)
 # y = ffn.forward(x, 512, 1024)
 
 
+# model = DepthModel(img_size=[512,1024])
+# y = model.forward(x)
 
-att = Attention(dim=64, num_embeddings=64)
-y = att.forward(x, 128, 256)
+
+dp = DepthModel(img_size=[512,1024])
+y = dp.forward(x)
+
+# blk =Block(num_embeddings=64, hidden_features=256, sr_ratio=8)
+# y = blk.forward(x, 128, 256)
 
 print(y.shape)
+
+#att = Attention(num_embeddings=64, sr_ratio=4, num_heads=1)
+#y = att.forward(x, 128, 256)
+
+# att = SegAttention(dim=64, sr_ratio=8, num_heads=1)
+# z = att.forward(y[0], y[1], y[2])
+
+# print(z.shape)
 
 #depth.forward(x)
 
@@ -42,7 +84,7 @@ print(y.shape)
 # y, HW =pe.forward(x)
 
 # print("Here")
-# print(y.shape)
+print(y.shape)
 # print(HW)
 
 
